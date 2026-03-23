f5b2ffce28d98ebd080c36a13e72f5d2a43a3108
Filip Skawina <skawina_filip@ckziu2.edu.pl>
2026-03-23 10:10:47 +0100

    Zadanie 1 gotowe

---
+++ java-oop-simple/src/java/pl/oop/Kalkulator.java
@@ -0,0 +1,13 @@
+public class Kalkulator {
+ public double add(double a, double b) {
+ return a + b;
+ }
+ public double subtract(double a, double b) { return 0; }
+ public double multiply(double a, double b) { return 0; }
+ public double divide(double a, double b) {
+	if(b != 0){
+		return a/b;
+} else{
+	return 0;
+}
+}}
